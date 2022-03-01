0b198175933be6c8dac001a9e1e616ecc6af9235
Justin Ceronio <>
2022-03-01 11:04:08 +0100

    Add Firebase

--- marketplace/src/app/app.module.ts
+++ marketplace/src/app/app.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVaaVZgiDCOLY7GOyVtsqEYlcstEqCKA4",
+  authDomain: "gibb-marketplace.firebaseapp.com",
+  projectId: "gibb-marketplace",
+  storageBucket: "gibb-marketplace.appspot.com",
+  messagingSenderId: "637376662704",
+  appId: "1:637376662704:web:5d667adc1c5241711f7f75",
+  measurementId: "G-L2Q8M4YPKB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
