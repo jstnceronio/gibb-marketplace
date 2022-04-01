facaaa228402d3a9f4a8829183c0c6328a666246
Ceronio Justin BIT <justin.ceronio@bit.admin.ch>
2022-04-01 17:31:01 +0200

    Add registration component

--- marketplace/src/app/app-routing/app-routing.module.ts
+++ marketplace/src/app/app-routing/app-routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import {DashboardComponent} from "../pages/dashboard/dashboard.component";
 import {AuthGuard} from "../services/auth.guard";
+import {RegisterComponent} from "../pages/register/register.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent, canActivate: [AuthGuard],
+    component: DashboardComponent, canActivate: [AuthGuard]
   },
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent, canActivate: [AuthGuard]
   },
 ];
 
