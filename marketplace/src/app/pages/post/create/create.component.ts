ce606b06d8a09c0153706525c6e561d197473720
RoadJDK <realroadjdk@gmail.com>
2022-05-31 15:22:03 +0200

    add comment logic

--- marketplace/src/app/pages/post/create/create.component.ts
+++ marketplace/src/app/pages/post/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ReactiveFormsModule, FormBuilder, FormControl, FormGroup, NgForm, Validators, } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, } from "@angular/forms";
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 
