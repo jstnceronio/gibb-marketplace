3d49328be4e5e2366d56cdd23f83195e15553cd3
RoadJDK <realroadjdk@gmail.com>
2022-05-31 09:31:16 +0200

    read id param

--- marketplace/src/app/pages/post/single-view/post-view/post-view.component.ts
+++ marketplace/src/app/pages/post/single-view/post-view/post-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-view',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-view.component.scss']
 })
 export class PostViewComponent implements OnInit {
+  private routeSub: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      console.log(params)
+      console.log(params['id'])
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 
 }
